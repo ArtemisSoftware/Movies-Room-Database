4a92288c7cb7f6e8dfce941c2945cc2f6a233dcd
ArtemisSoftware <gustavo.isaac@gmail.com>
2020-03-19 18:41:59 +0000

    MainActivity

--- app/src/main/java/com/artemisSoftware/movieroomdb/movie/MoviesListFragment.java
+++ app/src/main/java/com/artemisSoftware/movieroomdb/movie/MoviesListFragment.java
@@ -6,6 +6,10 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,10 +20,10 @@ import com.artemisSoftware.movieroomdb.R;
 
 public class MoviesListFragment extends Fragment {
 
-    /*
+
     private MoviesListAdapter moviesListAdapter;
     private MoviesViewModel moviesViewModel;
-    */
+
     private Context context;
 
 
@@ -31,43 +35,50 @@ public class MoviesListFragment extends Fragment {
     public void onAttach(Context context) {
         super.onAttach(context);
         this.context = context;
-        //--moviesListAdapter = new MoviesListAdapter(context);
+        moviesListAdapter = new MoviesListAdapter(context);
     }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //--initData();
+        initData();
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
         View view = inflater.inflate(R.layout.fragment_movies_list, container, false);
-/*        RecyclerView recyclerView = view.findViewById(R.id.recyclerview_movies);
+
+        RecyclerView recyclerView = view.findViewById(R.id.recyclerview_movies);
         recyclerView.setAdapter(moviesListAdapter);
-        recyclerView.addItemDecoration(new DividerItemDecoration(context, DividerItemDecoration.VERTICAL));
+        //--recyclerView.addItemDecoration(new DividerItemDecoration(context, DividerItemDecoration.VERTICAL));
         recyclerView.setLayoutManager(new LinearLayoutManager(context));
-*/
+
         return view;
     }
 
-    /*
+
     private void initData() {
+
         moviesViewModel = ViewModelProviders.of(this).get(MoviesViewModel.class);
+        /*
         moviesViewModel.getMoviesList().observe(this, new Observer<List<Movie>>() {
             @Override
             public void onChanged(@Nullable List<Movie> movies) {
                 moviesListAdapter.setMovieList(movies);
             }
         });
+         */
     }
 
     public void removeData() {
+        /*
         if (moviesListAdapter != null) {
             moviesViewModel.deleteAll();
         }
+        */
     }
-    */
+
 }
