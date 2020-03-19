9e35ff79ceed25b0a5293a6da9c2c45f815af6a3
ArtemisSoftware <gustavo.isaac@gmail.com>
2020-03-19 19:01:39 +0000

    DirectorDao

---
+++ app/src/main/java/com/artemisSoftware/movieroomdb/db/DirectorDao.java
@@ -0,0 +1,35 @@
+package com.artemisSoftware.movieroomdb.db;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface DirectorDao {
+
+    @Query("SELECT * FROM director WHERE did = :id LIMIT 1")
+    Director findDirectorById(int id);
+
+    @Query("SELECT * FROM director WHERE full_name = :fullName LIMIT 1")
+    Director findDirectorByName(String fullName);
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    long insert(Director director);
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(Director... directors);
+
+    @Update(onConflict = OnConflictStrategy.IGNORE)
+    void update(Director director);
+
+    @Query("DELETE FROM director")
+    void deleteAll();
+
+    @Query("SELECT * FROM director ORDER BY full_name ASC")
+    LiveData<List<Director>> getAllDirectors();
+}
