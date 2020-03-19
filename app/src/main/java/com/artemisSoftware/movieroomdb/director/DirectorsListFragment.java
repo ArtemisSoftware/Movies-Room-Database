64f91784764ff43bcbe492359ae0fbc8f5c6ded6
gustavo <gustavo.maia@b2b.com.pt>
2020-03-19 10:26:52 +0000

    DirectorsListFragment

--- app/src/main/java/com/artemisSoftware/movieroomdb/director/DirectorsListFragment.java
+++ app/src/main/java/com/artemisSoftware/movieroomdb/director/DirectorsListFragment.java
@@ -4,7 +4,11 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,100 +16,62 @@ import android.view.ViewGroup;
 
 import com.artemisSoftware.movieroomdb.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link DirectorsListFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link DirectorsListFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class DirectorsListFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private DirectorsListAdapter directorsListAdapter;
+    //--private DirectorsViewModel directorsViewModel;
+    private Context context;
 
-    private OnFragmentInteractionListener mListener;
-
-    public DirectorsListFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment DirectorsListFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static DirectorsListFragment newInstance(String param1, String param2) {
-        DirectorsListFragment fragment = new DirectorsListFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_directors, container, false);
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
+    public static DirectorsListFragment newInstance() {
+        return new DirectorsListFragment();
     }
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
+        this.context = context;
+        directorsListAdapter = new DirectorsListAdapter(context);
     }
 
     @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        initData();
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_directors, container, false);
+
+        RecyclerView recyclerView = view.findViewById(R.id.recyclerview_directors);
+        recyclerView.setAdapter(directorsListAdapter);
+        recyclerView.addItemDecoration(new DividerItemDecoration(context, DividerItemDecoration.VERTICAL));
+        //--recyclerView.setLayoutManager(new LinearLayoutManager(context));
+        return view;
+    }
+
+    private void initData() {
+        /*
+        directorsViewModel = ViewModelProviders.of(this).get(DirectorsViewModel.class);
+        directorsViewModel.getDirectorList().observe(this, new Observer<List<Director>>() {
+            @Override
+            public void onChanged(@Nullable List<Director> directors) {
+                directorsListAdapter.setDirectorList(directors);
+            }
+        });
+        */
+    }
+
+    public void removeData() {
+        /*
+        if (directorsViewModel != null) {
+            directorsViewModel.deleteAll();
+        }
+
+         */
     }
 }
