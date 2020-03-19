2da1e69b6280ae0b021c0f80753183a566889110
gustavo <gustavo.maia@b2b.com.pt>
2020-03-19 12:31:25 +0000

    MoviesViewModel

---
+++ app/src/main/java/com/artemisSoftware/movieroomdb/MoviesViewModel.java
@@ -0,0 +1,41 @@
+package com.artemisSoftware.movieroomdb;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.artemisSoftware.movieroomdb.db.Movie;
+
+import java.util.List;
+
+public class MoviesViewModel extends AndroidViewModel {
+
+    //private MovieDao movieDao;
+    private LiveData<List<Movie>> moviesLiveData;
+
+    public MoviesViewModel(@NonNull Application application) {
+        super(application);
+        //movieDao = MoviesDatabase.getDatabase(application).movieDao();
+        //moviesLiveData = movieDao.getAllMovies();
+    }
+
+    public LiveData<List<Movie>> getMoviesList() {
+        return moviesLiveData;
+    }
+
+    /*
+    public void insert(Movie... movies) {
+        movieDao.insert(movies);
+    }
+
+    public void update(Movie movie) {
+        movieDao.update(movie);
+    }
+
+    public void deleteAll() {
+        movieDao.deleteAll();
+    }
+    */
+}
