bd45afe534e33782f418076b0bf8ed70b3b83dcf
ArtemisSoftware <gustavo.isaac@gmail.com>
2020-03-19 20:29:53 +0000

    Movie entity

--- app/src/main/java/com/artemisSoftware/movieroomdb/db/Movie.java
+++ app/src/main/java/com/artemisSoftware/movieroomdb/db/Movie.java
@@ -1,32 +1,28 @@
 package com.artemisSoftware.movieroomdb.db;
 
 import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 
-/*
-@Entity(tableName = "movie",
+
+@Entity(tableName = "movie"/*,
         foreignKeys = @ForeignKey(entity = Director.class,
                 parentColumns = "did",
                 childColumns = "directorId",
                 onDelete = ForeignKey.CASCADE),
-        indices = {@Index("title"), @Index("directorId")})
-*/
+        indices = {@Index("title"), @Index("directorId")}*/)
 public class Movie {
 
-    /*
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "mid")
-    */
     public int id;
 
-    /*
     @ColumnInfo(name = "title")
     @NonNull
-     */
     public String title;
 
-    /*
     @ColumnInfo(name = "directorId")
-     */
     public int directorId;
 
     public Movie(@NonNull String title, int directorId) {
