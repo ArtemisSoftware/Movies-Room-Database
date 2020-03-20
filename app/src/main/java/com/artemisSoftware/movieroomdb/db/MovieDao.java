3e07eb1e4a6606d8a08afb34ba1378611827eb04
gustavo <gustavo.maia@b2b.com.pt>
2020-03-20 09:42:52 +0000

    MovieDao

---
+++ app/src/main/java/com/artemisSoftware/movieroomdb/db/MovieDao.java
@@ -0,0 +1,29 @@
+package com.artemisSoftware.movieroomdb.db;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface MovieDao {
+
+    @Query("SELECT * FROM movie WHERE title = :title LIMIT 1")
+    Movie findMovieByTitle(String title);
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(Movie... directors);
+
+    @Update(onConflict = OnConflictStrategy.IGNORE)
+    void update(Movie director);
+
+    @Query("DELETE FROM movie")
+    void deleteAll();
+
+    @Query("SELECT * FROM movie ORDER BY title ASC")
+    LiveData<List<Movie>> getAllMovies();
+}
